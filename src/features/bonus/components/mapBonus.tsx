42fc443e64e461a8c55f37b993feb4e0f10abdb8
WoundedPaintJob <baladalidila@pm.me>
2023-09-12 14:23:53 +0200

    Draw bonus shapes

--- src/features/bonus/components/mapBonus.tsx
+++ src/features/bonus/components/mapBonus.tsx
@@ -10,34 +10,42 @@ const MapBonus = (props: { bonus: BonusState }) => {
   const setActive = useLevelStore((state) => state.SetActiveBonus);
   const assetSize = 40;
   const drawImage = shouldDrawImage(props.bonus.Reward, renderSettings);
-  if (drawImage && !props.bonus.Conquered)
-    return (
-      <image
-        key={`${props.bonus.Id}Ass`}
-        href={`${Settings.RewardUrl}${props.bonus.Reward.Image}`}
-        x={props.bonus.Svg.Center.X - assetSize / 2}
-        y={props.bonus.Svg.Center.Y - assetSize / 2}
-        width={assetSize}
-        height={assetSize}
-        onClick={() => setActive(props.bonus)}
-      />
-    );
-  if (
-    renderSettings.get(RenderOptionType.MoneyPerSecond) &&
-    props.bonus.Reward.MoneyPerSecond
-  ) {
-    return (
-      <text
-        key={`${props.bonus.Id}Text`}
-        x={props.bonus.Svg.Center.X - 8}
-        y={props.bonus.Svg.Center.Y + 2}
-        fontWeight={400}
-        fontSize={10}
-        onClick={() => setActive(props.bonus)}
-      >
-        {formatNumber(props.bonus.Reward.MoneyPerSecond)}
-      </text>
-    );
-  }
+  return (
+    <g key={`Z${props.bonus.Id}G`} onClick={() => setActive(props.bonus)}>
+      {props.bonus.Svg.Shapes.map((sh, index) => (
+        <polygon
+          key={`B${props.bonus.Id}${index}Pol`}
+          id={`B${props.bonus.Id}`}
+          points={sh.Path}
+          stroke="#000"
+          fill="#000"
+        />
+      ))}
+      {drawImage && !props.bonus.Conquered && (
+        <image
+          key={`${props.bonus.Id}Ass`}
+          href={`${Settings.RewardUrl}${props.bonus.Reward.Image}`}
+          x={props.bonus.Svg.Center.X - assetSize / 2}
+          y={props.bonus.Svg.Center.Y - assetSize / 2}
+          width={assetSize}
+          height={assetSize}
+          onClick={() => setActive(props.bonus)}
+        />
+      )}
+      {renderSettings.get(RenderOptionType.MoneyPerSecond) &&
+        props.bonus.Reward.MoneyPerSecond && (
+          <text
+            key={`${props.bonus.Id}Text`}
+            x={props.bonus.Svg.Center.X - 8}
+            y={props.bonus.Svg.Center.Y + 2}
+            fill="#FFF"
+            fontWeight={400}
+            fontSize={10}
+          >
+            {formatNumber(props.bonus.Reward.MoneyPerSecond)}
+          </text>
+        )}
+    </g>
+  );
 };
 export default MapBonus;
--- src/features/zone/components/mapZone.tsx
+++ src/features/zone/components/mapZone.tsx
@@ -23,7 +23,9 @@ const MapZone = (props: {
 
   let hospitalSaves = 0;
   props.conqueredHospitals.forEach(
-    (h) => (hospitalSaves += hospitalSaveForZone(h, props.zone) * props.hospitalMultiplier)
+    (h) =>
+      (hospitalSaves +=
+        hospitalSaveForZone(h, props.zone) * props.hospitalMultiplier)
   );
   const assetSize = 40;
   if (!props.zone) return <></>;
@@ -38,53 +40,51 @@ const MapZone = (props: {
   );
   const drawImage = shouldDrawImage(props.zone.Reward, renderSettings);
   return (
-    <>
+    <g
+      key={`Z${props.zone.Id}G`}
+      onClick={() => setActive(props.zone)}
+      onContextMenu={(e) => {
+        e.preventDefault();
+        setConquered(props.zone);
+      }}
+    >
       {props.zone.Svg.Shapes.map((sh, index) => {
         return (
-          <g
-            key={index}
-            onClick={() => setActive(props.zone)}
-            onContextMenu={(e) => {
-              e.preventDefault();
-              setConquered(props.zone);
-            }}
-          >
-            <polygon
-              key={`${props.zone.Id}Pol`}
-              id={`${props.zone.Id}`}
-              points={sh.Path}
-              stroke="#000"
-              fill={color}
-            />
-            {drawImage && !props.zone.Conquered && (
-              <image
-                key={`${props.zone.Id}Ass`}
-                href={`${Settings.RewardUrl}${props.zone.Reward.Image}`}
-                x={props.zone.Center.X - assetSize / 2}
-                y={props.zone.Center.Y - assetSize / 2}
-                width={assetSize}
-                height={assetSize}
-              />
-            )}
-            {renderSettings.get(RenderOptionType.Cost) && (
-              <text
-                key={`${props.zone.Id}Text`}
-                x={props.zone.Center.X - 20}
-                y={props.zone.Center.Y}
-                paintOrder="stroke"
-                stroke="#FFF"
-                strokeWidth="1px"
-                strokeLinecap="butt"
-                strokeLinejoin="miter"
-                fontWeight={400}
-              >
-                {formatNumber(props.zone.Cost)}
-              </text>
-            )}
-          </g>
+          <polygon
+            key={`${props.zone.Id}${index}Pol`}
+            id={`${props.zone.Id}${index}`}
+            points={sh.Path}
+            stroke="#000"
+            fill={color}
+          />
         );
       })}
-    </>
+      {drawImage && !props.zone.Conquered && (
+        <image
+          key={`${props.zone.Id}Ass`}
+          href={`${Settings.RewardUrl}${props.zone.Reward.Image}`}
+          x={props.zone.Center.X - assetSize / 2}
+          y={props.zone.Center.Y - assetSize / 2}
+          width={assetSize}
+          height={assetSize}
+        />
+      )}
+      {renderSettings.get(RenderOptionType.Cost) && (
+        <text
+          key={`${props.zone.Id}Text`}
+          x={props.zone.Center.X - 20}
+          y={props.zone.Center.Y}
+          paintOrder="stroke"
+          stroke="#FFF"
+          strokeWidth="1px"
+          strokeLinecap="butt"
+          strokeLinejoin="miter"
+          fontWeight={400}
+        >
+          {formatNumber(props.zone.Cost)}
+        </text>
+      )}
+    </g>
   );
 };
 export default MapZone;
